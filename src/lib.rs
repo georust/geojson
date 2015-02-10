0d8263644fc47d76876b2f16cac6da08c475d654
Corey Farwell <coreyf@rwell.org>
2015-02-09 22:44:26 -0500

    Silence warnings, enable 'core' feature

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![feature(core)]
 
 extern crate "rustc-serialize" as rustc_serialize;
 
