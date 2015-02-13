e4de953c72f2696e63663b87f8e003635bc5a886
Corey Farwell <coreyf@rwell.org>
2015-02-13 10:02:44 -0500

    Remove unknown feature

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(core)]
-
 extern crate "rustc-serialize" as rustc_serialize;
 
 pub use types::pos::Pos;
