1bf8cea16b0c4428457065d5f42c20b4bf45e906
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2017-12-21 12:12:24 +0000

    Add a more comprehensive parsing example

--- src/lib.rs
+++ src/lib.rs
@@ -95,6 +95,92 @@
 //! let geojson_string = geojson.to_string();
 //! # }
 //! ```
+//!
+//! ## Parsing
+//! 
+//! GeoJSON's [spec](https://tools.ietf.org/html/rfc7946) is quite simple, but
+//! it has several subtleties that must be taken into account when parsing it:  
+//! 
+//! - The `geometry` field of a `Feature` is an `Option`
+//! - `GeometryCollection`s contain other `Geometry` objects, and can nest.
+//!
+//! Here's a minimal example which will parse valid GeoJSON without taking
+//! ownership of it.
+//!
+//! ```rust
+//! use geojson::{GeoJson, Geometry, Value};
+//! 
+//! /// Process GeoJSON geometries  
+//! fn match_geometry(geom: &Geometry) {
+//!     match &geom.value {
+//!         &Value::Polygon(_) => println!("Matched a Polygon"),
+//!         &Value::MultiPolygon(_) => println!("Matched a MultiPolygon"),
+//!         &Value::GeometryCollection(ref gc) => {
+//!             println!("Matched a GeometryCollection");
+//!             // GeometryCollections contain other Geometry types, and can nest
+//!             // we deal with this by recursively processing each geometry
+//!             for geometry in gc {
+//!                 match_geometry(geometry)
+//!             }
+//!         }
+//!         // Point, LineString, and their Multi– counterparts
+//!         _ => println!("Matched some other geometry"),
+//!     }
+//! }
+//! 
+//! /// Process top-level GeoJSON items
+//! fn process_geojson(gj: &GeoJson) {
+//!     match gj {
+//!         &GeoJson::FeatureCollection(ref ctn) => for feature in &ctn.features {
+//!             if let &Some(ref geom) = &feature.geometry {
+//!                 match_geometry(&geom)
+//!             }
+//!         },
+//!         &GeoJson::Feature(ref feature) => {
+//!             if let &Some(ref geom) = &feature.geometry {
+//!                 match_geometry(&geom)
+//!             }
+//!         }
+//!         &GeoJson::Geometry(ref geometry) => match_geometry(&geometry),
+//!     }
+//! }
+//! 
+//! fn main() {
+//!     let geojson_str = r#"
+//!     {
+//!       "type": "GeometryCollection",
+//!       "geometries": [
+//!         {"type": "Point", "coordinates": [0,1]},
+//!         {"type": "MultiPoint", "coordinates": [[-1,0],[1,0]]},
+//!         {"type": "LineString", "coordinates": [[-1,-1],[1,-1]]},
+//!         {"type": "MultiLineString", "coordinates": [
+//!           [[-2,-2],[2,-2]],
+//!           [[-3,-3],[3,-3]]
+//!         ]},
+//!         {"type": "Polygon", "coordinates": [
+//!           [[-5,-5],[5,-5],[0,5],[-5,-5]],
+//!           [[-4,-4],[4,-4],[0,4],[-4,-4]]
+//!         ]},
+//!         { "type": "MultiPolygon", "coordinates": [[
+//!           [[-7,-7],[7,-7],[0,7],[-7,-7]],
+//!           [[-6,-6],[6,-6],[0,6],[-6,-6]]
+//!         ],[
+//!           [[-9,-9],[9,-9],[0,9],[-9,-9]],
+//!           [[-8,-8],[8,-8],[0,8],[-8,-8]]]
+//!         ]},
+//!         {"type": "GeometryCollection", "geometries": [
+//!           {"type": "Polygon", "coordinates": [
+//!             [[-5.5,-5.5],[5,-5],[0,5],[-5,-5]],
+//!             [[-4,-4],[4,-4],[0,4],[-4.5,-4.5]]
+//!           ]}
+//!         ]}
+//!       ]
+//!     }
+//!     "#;
+//!     let geojson = geojson_str.parse::<GeoJson>().unwrap();
+//!     process_geojson(&geojson);
+//! }
+//! ```
 
 extern crate serde;
 #[macro_use]
