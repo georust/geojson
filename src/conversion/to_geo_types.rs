6ca77363c14a9b91ec1dd58b98a44a48f1f2fcd4
Stephan Hügel <shugel@tcd.ie>
2020-09-03 18:28:26 +0100

    Fix feature labels for docs.rs
    See here: https://stackoverflow.com/a/61417700/416626

--- src/conversion/to_geo_types.rs
+++ src/conversion/to_geo_types.rs
@@ -1,3 +1,7 @@
+// only enables the `doc_cfg` feature when
+// the `docsrs` configuration attribute is defined
+#[cfg_attr(docsrs, feature(doc_cfg))]
+
 use crate::geo_types;
 
 use crate::geometry;
