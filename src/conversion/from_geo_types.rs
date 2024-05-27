53f540a24f0868c9b8b50bfa13393ce94161df4d
Nick Kocharhook <nick@kocharhook.com>
2024-05-27 05:36:13 +0100

    Fix deprecation warnings (#242)
    * geo_types::Coordinate -> Coord
    
    * Remove unused imports
    
    * deprecated points_iter() -> plain points()
    
    * Bump geo-types min version.

--- src/conversion/from_geo_types.rs
+++ src/conversion/from_geo_types.rs
@@ -194,7 +194,7 @@ where
     T: CoordFloat,
 {
     line_string
-        .points_iter()
+        .points()
         .map(|point| create_point_type(&point))
         .collect()
 }
@@ -242,7 +242,7 @@ where
 {
     let mut coords = vec![polygon
         .exterior()
-        .points_iter()
+        .points()
         .map(|point| create_point_type(&point))
         .collect()];
 
@@ -271,8 +271,8 @@ where
 mod tests {
     use crate::{GeoJson, Geometry, Value};
     use geo_types::{
-        Coordinate, GeometryCollection, Line, LineString, MultiLineString, MultiPoint,
-        MultiPolygon, Point, Polygon, Rect, Triangle,
+        Coord, GeometryCollection, Line, LineString, MultiLineString, MultiPoint, MultiPolygon,
+        Point, Polygon, Rect, Triangle,
     };
 
     #[test]
@@ -356,9 +356,9 @@ mod tests {
 
     #[test]
     fn geo_triangle_conversion_test() {
-        let c1 = Coordinate { x: 0., y: 0. };
-        let c2 = Coordinate { x: 10., y: 20. };
-        let c3 = Coordinate { x: 20., y: -10. };
+        let c1 = Coord { x: 0., y: 0. };
+        let c2 = Coord { x: 10., y: 20. };
+        let c3 = Coord { x: 20., y: -10. };
 
         let triangle = Triangle(c1, c2, c3);
 
@@ -381,8 +381,8 @@ mod tests {
 
     #[test]
     fn geo_rect_conversion_test() {
-        let c1 = Coordinate { x: 0., y: 0. };
-        let c2 = Coordinate { x: 10., y: 20. };
+        let c1 = Coord { x: 0., y: 0. };
+        let c2 = Coord { x: 10., y: 20. };
 
         let rect = Rect::new(c1, c2);
 
